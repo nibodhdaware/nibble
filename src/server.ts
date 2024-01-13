447f666c28924c0385a17cf987f2bd54cb90b1df
Nibodh Daware <55696034+nibodhdaware@users.noreply.github.com>
2024-01-13 09:46:34 +0530

    added keepAlive

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import express from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Running!");
+  res.send("Running!");
 });
 
 function keepAlive() {
-    app.listen(3000, () => {
-        console.log("Server is ready.");
-    });
+  app.listen(port, () => {
+    console.log("Server is ready.");
+  });
 }
 
 export default keepAlive;
