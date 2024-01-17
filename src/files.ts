572ece2a1db6095249bb51ada28172b1c8a5321d
Nibodh Daware <55696034+nibodhdaware@users.noreply.github.com>
2024-01-17 17:31:50 +0530

    updated file paths for deployment

--- src/files.ts
+++ src/files.ts
@@ -1,11 +1,14 @@
 import fs from "fs";
+import path from "path";
 
+const commandsFolder = path.join(__dirname, "./commands");
 const commandFiles = fs
-    .readdirSync("./src/commands")
+    .readdirSync(commandsFolder)
     .filter((file) => file.endsWith(".ts"));
 
+const eventsFolder = path.join(__dirname, "./events");
 const eventFiles = fs
-    .readdirSync("./src/events")
+    .readdirSync(eventsFolder)
     .filter((file) => file.endsWith(".ts"));
 
 export { commandFiles, eventFiles };
